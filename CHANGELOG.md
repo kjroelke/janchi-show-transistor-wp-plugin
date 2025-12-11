9d71c0446c50568f08e1c938a4a085ddcdd5b362
KJ Roelke <kj.roelke@gmail.com>
2025-12-10 22:47:03 -0600

    Update version to 1.0.1, enhance changelog, and fix episode syncing logic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This plugin follows [semver](https://semver.org/)
 
+## v1.0.1 - [December 10, 2025]
+
+-   Fixed: Removed a line that prevented batch syncing of episodes.
+
 ## v1.0.0
 
 -   Init plugin
--- janchi-show-transistor-wp-plugin.php
+++ janchi-show-transistor-wp-plugin.php
@@ -3,14 +3,14 @@
  * Plugin Name: Janchi Show x Transistor WP Plugin
  * Plugin URI: https://github.com/choctaw-nation/cno-template-plugin
  * Description: Keeps the Janchi Show website in sync with Transistor.fm episodes.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: K.J. Roelke
  * Author URI: https://www.kjroelke.online
  * License: GPLv3 or later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Requires PHP: 8.2
  * Requires at least: 6.7.0
- * Tested up to: 6.8.3
+ * Tested up to: 6.9.0
  *
  * @package JanchiShow
  */
